786647b404c8f8d5864970f2dba5cbf336d46984
Ferencz Veres <ferenczv8@gmail.com>
2015-12-11 01:12:29 +0200

    added initValues cv

--- initValues.php
+++ initValues.php
@@ -8,7 +8,8 @@
 //session_unset();
 if(!isset($_SESSION['candidates'])){
     $user = new User(time(), 'ferencz', '123456');
-    $candidate = new Candidate(time(), 'ferencz', 'veres', '2010-03-21', 'area', '0123456789', 'ferenczv@gmail.com', '0', $user);
+    $cv = new CV(time(), 'junior', array(new Education(time(), 'BV', 'science', null, null)), array(new ProfessionalExperience(time(), 'BV', 'Test', null, null, null)));
+    $candidate = new Candidate(time(), 'ferencz', 'veres', '2010-03-21', 'area', '0123456789', 'ferenczv@gmail.com', '0', $user, $cv);
     $_SESSION['candidates'] = array($candidate);
 }
 ?>
