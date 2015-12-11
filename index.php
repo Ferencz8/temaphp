a7cbc5e1d777564d871658b2d613778a0c27235e
Marius Partoaca <marius@spx73-nbasus>
2015-12-11 08:13:41 +0200

    Fix uppercase file names for linux

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ require_once 'models/User.php';
 require_once 'models/CV.php'; //problema serializare, il putem scoate dupa
 require_once 'models/Education.php'; //problema serializare, il putem scoate dupa
 require_once 'models/ProfessionalExperience.php'; //problema serializare, il putem scoate dupa
-require_once 'dal/CandidateRepository.php';
-require_once 'dal/CompanyRepository.php';
-require_once 'dal/CVRepository.php';
+require_once 'dal/candidateRepository.php';
+require_once 'dal/companyRepository.php';
+require_once 'dal/cvRepository.php';
 session_start();
 require_once('core/connection.php');
 require_once('core/autoloader.php');
